cdd16d4a90e77f0bbc81b81df20348c055959d86
Aexer <302801376@student.rocmondriaan.nl>
2022-03-11 11:52:02 +0100

    filter todo gedaan js in progress enter input lukt

--- js/main.js
+++ js/main.js
@@ -3,15 +3,17 @@ console.log("main.js loaded");
 const addTodoBtn = document.querySelector(".add-todo");
 const checkTodoBtn = document.querySelector(".list-done");
 const todoInput = document.querySelector(".todo-input");
+const list = document.querySelector(".todo-list");
+const input = document.querySelector(".input");
+const filterSelect = document.querySelector(".filter-todos");
 
 addTodoBtn.addEventListener("click", addTodo);
+todoInput.addEventListener("keyup", enterTodo);
+filterSelect.addEventListener("click", filterTodos)
 
 function addTodo(event) {
-  event.preventDefault();
-  // ^Prevent form from submitting^
-  const list = document.querySelector(".todo-list");
-  const input = document.querySelector(".input");
-  // get Todo List and input value
+  event.preventDefault()
+  // Prevent
   const todoListItem = document.createElement("li");
   todoListItem.classList.add("list-item")
   // ^Create Todo List Item^
@@ -65,5 +67,29 @@ function checkTodo(e) {
   } else {
     e.target.parentElement.children[0].classList.add("todo-checked")
     // ^if it doesnt have todo-checked add todo-checked^
+  }
+}
+
+function filterTodos(e) {
+  const todos = list.childNodes;
+  todos.forEach(function(todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+    }
+  })
+}
+
+function enterTodo(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    addTodoBtn.click();
   }
 }
